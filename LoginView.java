e80c08ac784850477870b3ea57f889ab9d4017f0
Mustafa <bawim@student.chalmers.se>
2023-03-15 20:35:58 +0100

    Skeleton code.

---
+++ LoginView.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class LoginView {
+    public JFrame frame;
+    public JTextField username;
+    public JPasswordField passwordField;
+
+    public static void main(String[] args){
+
+    }
+
+    public LoginView(){
+
+    }
+
+
+}
